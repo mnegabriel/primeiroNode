b89a9790f80a0cb17a051b4ba901dea17fc6e009
mnegabriel <gabrielmd123@gmail.com>
2020-06-21 16:27:17 -0300

    New http requests implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,13 @@ app.use(express.json());
 const projects = [];
 
 app.get('/projects', (req, res) => {
-    // const query = req.query;
-    // console.log(query);
+    const { title } = req.query;
 
-    return res.json(projects);
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects
+
+    return res.json(results);
 });
 
 app.post('/projects', (req, res) => {
@@ -25,26 +28,31 @@ app.post('/projects', (req, res) => {
 
 app.put('/projects/:id', (req, res) => {
     const { id } = req.params
+    const { title, owner } = req.body;
+
     const projectIndex = projects.findIndex(project => project.id == id);
 
     if (projectIndex < 0) {
         return res.status(400).json({ error: 'Project not found' })
     }
 
+    const project = { id, title, owner }
+    projects[projectIndex] = project
 
-
-    return res.json([
-        'Project 4',
-        'Project 2',
-        'Peoject 3'
-    ])
+    return res.json(project)
 });
 
 app.delete('/projects/:id', (req, res) => {
-    return res.json([
-        'Project 2',
-        'Peoject 3'
-    ])
+    const { id } = req.params
+
+    const projectIndex = projects.findIndex(project => project.id == id)
+
+    if (projectIndex < 0) {
+        return res.status(400).json({ error: 'Project not found' })
+    }
+
+    projects.splice(projectIndex, 1)
+    return res.status(204).send()
 });
 
 app.listen(3333, () => {
